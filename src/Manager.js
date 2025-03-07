7117cf574f91541f975dbeb4a12781336aa31e15
Andrew Scoppa <andrew-scoppa@github.com>
2025-03-07 15:27:20 -0500

    Updated

--- src/Manager.js
+++ src/Manager.js
@@ -2,15 +2,8 @@
 const Employee = require('./Employee');
 
 
-/*
-Create a class Manager that has the following properties:
-•	department - string
-The class should inherit the properties of the Employee class and have the following methods:
-•	getDetails() - a method that returns a string, containing information about the manager
-Requirements:
-•	The Manager class should be exported as a module
-*/
 class Manager extends Employee {
+    // TODO Implement error handling for the constructor. 
     constructor(id, name, salary, department) {
         super(id, name, salary);
         this.department = department;
@@ -19,6 +12,10 @@ class Manager extends Employee {
     getDetails() {
         return `${super.getDetails()} and manages the ${this.department} department`;
     }
+
+    // TODO: Implement a getDepartment method. It should return the department of the manager.
+    
+
 }
 
 module.exports = Manager;
